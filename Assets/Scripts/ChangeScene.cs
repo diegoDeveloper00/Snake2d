3428ada59484e6c1cb03738fae46a64437d888e2
diegoDeveloper00 <diegoDeveloper00@users.noreply.github.com>
2020-10-26 17:32:33 +0100

    creation basic scene transition

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        if (SceneManager.GetActiveScene().buildIndex.Equals(1))
+        {
+            anim.SetBool("AnimateIn", true);
+        }
+    }
+
+    public void playGame()
+    {
+        StartCoroutine(loadLevel());
+
+    }
+
+    IEnumerator loadLevel()
+    {
+        anim.SetBool("animateOut", true);
+
+        yield return new WaitForSeconds(1f);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -33,7 +34,7 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        highScore = PlayerPrefs.GetInt("hishScore");
+        highScore = PlayerPrefs.GetInt("highScore");
     }
 
     public void updateHighScore()
@@ -44,6 +45,21 @@ public class GameManager : MonoBehaviour
 
             PlayerPrefs.SetInt("highScore", highScore);
         }
+    }
+
+    public void reloadGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void playGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void quiGame()
+    {
+        Application.Quit();
     }
 
 }
